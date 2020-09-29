5c543a15f88a9e4c088807c976733c54ee1306ca
huawei.william <huawei.william@bytedance.com>
2020-09-29 13:49:23 +0800

    add stream_format_tester.cpp

---
+++ ch03/stream_format_tester.cpp
@@ -0,0 +1,46 @@
+
+//
+// Created by William.Hua on 2020/9/29.
+//
+#include <AudioToolbox/AudioToolbox.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    AudioFileTypeAndFormatID  file_type_and_format_id;
+    file_type_and_format_id.mFileType = kAudioFileAIFFType;
+    file_type_and_format_id.mFormatID = kAudioFormatLinearPCM;
+
+    OSStatus err = noErr;
+    UInt32 info_size = 0;
+
+    err = AudioFileGetGlobalInfoSize(kAudioFileGlobalInfo_AvailableStreamDescriptionsForFormat,
+                                     sizeof(file_type_and_format_id),
+                                     &file_type_and_format_id,
+                                     &info_size);
+    assert(err == noErr);
+    cout << "info size: " << info_size << endl;
+
+    auto  *asbds = (AudioStreamBasicDescription*)malloc(info_size);
+    err = AudioFileGetGlobalInfo(kAudioFileGlobalInfo_AvailableStreamDescriptionsForFormat,
+                                 sizeof(file_type_and_format_id),
+                                 &file_type_and_format_id,
+                                 &info_size,
+                                 asbds);
+    assert(err == noErr);
+
+    int asbd_count = info_size / sizeof(AudioStreamBasicDescription);
+    cout << "asbd count: " << asbd_count << endl;
+
+    for(int i = 0; i < asbd_count; ++i){
+        UInt32 format4cc = CFSwapInt32HostToBig(asbds[i].mFormatID);
+        cout << i << ": mFormatId: "
+             << (char*)(&format4cc) << ", mFormatFlags: "
+             << asbds[i].mFormatFlags << ", mBitsPerChannel: "
+             << asbds[i].mBitsPerChannel << endl;
+    }
+
+    free(asbds);
+    return 0;
+}
